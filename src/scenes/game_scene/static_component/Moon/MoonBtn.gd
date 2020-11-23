05a69c4c5e95f11fb33abbf9ef944f5572cbe638
Zen-net <allyofjustice1@gmail.com>
2020-11-23 22:35:04 +0700

    Nicer moon

--- src/scenes/game_scene/static_component/Moon/MoonBtn.gd
+++ src/scenes/game_scene/static_component/Moon/MoonBtn.gd
@@ -8,11 +8,11 @@ func _ready():
 	self.connect("pressed",self,"_emit_orbit_signal")
 
 func _emit_orbit_signal():
-	var label = get_parent().find_node("StateLabel")
+	var label = get_parent().get_parent().find_node("StateLabel")
 	match RocketStates.currentState:
 		RocketStates.ATTACHED:
 			EventBus.emit_signal("attach_to_moon",[RocketStates.RELEASED,center])
-			self.get_children()[0].text = ''
+			label.text = ''
 		RocketStates.RELEASED:
 			EventBus.emit_signal("attach_to_moon",[RocketStates.ATTACHED,center])
-			self.get_children()[0].text = 'Attached'
+			label.text = 'Attached'
